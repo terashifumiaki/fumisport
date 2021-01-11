b7cb7872cda398fadb5180b6bd69786059ffa3fd
fumiaki.terashi <karakaraponpon@gmail.com>
2021-01-11 05:29:43 +0000

    first

---
+++ js/script.js
@@ -0,0 +1,72 @@
+// PhotoSwipe
+initPhotoSwipeFromDOM(".js-my-gallery");
+
+$(function () {
+
+  //iOS対策
+  //iOSでは疑似要素を含むaタグのリンクは２回タップしないと遷移とページ内スクロールをしないため、
+  //ユーザーエージェント判定でiOSの場合はbodyタグにiosを付与し、対象の疑似要素をdisplay: noneする
+  var ua = navigator.userAgent;
+  if (/iPhone|iPad|iPod/.test(ua)) {
+    $("body").addClass("ios");
+  }
+
+  //Worksのリンクを有効化
+  //スライド（Swiper）内に記載のリンクを有効にするため下記の記述が必要 (;´･ω･)ｳｰﾝ･･･
+  $(".works-url").on("click", "a", function (e) {
+    e.stopPropagation();
+  });
+
+  //ページ内スクロール
+  var $nav = $(".gnav");
+  var navHeight = $nav.outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate(
+      {
+        scrollTop: position,
+      },
+      300,
+      "swing"
+    );
+    return false;
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      300
+    );
+    return false;
+  });
+});
+
+// animatedクラスの付いた要素にwaypointを登録
+$('.animated').waypoint({
+  handler(direction) {
+    // 要素が画面中央に来るとここが実行される
+    if (direction === 'down') {
+      /**
+       * 下方向のスクロール
+       * イベント発生元の要素にfadeInUpクラスを付けることで
+       * アニメーションを開始
+       */
+      $(this.element).addClass('fadeInUp');
+
+      /**
+       * waypointを削除することで、この要素に対しては
+       * これ以降handlerが呼ばれなくなる
+       */
+      this.destroy();
+    }
+  },
+
+  // 要素が画面中央に来たらhandlerを実行
+  offset: '50%',
+});
